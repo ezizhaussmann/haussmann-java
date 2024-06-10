1c1ea997737b131c4a3c88eb825025e5f62e596f
haussmann <aziznehal1@gmail.com>
2024-06-10 16:39:37 +0200

    1****5424

---
+++ src/main/java/Bases/BaseClass.java
@@ -0,0 +1,19 @@
+package Bases;
+
+import org.magento.utilsclasses.Config;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseClass {
+    public static ChromeDriver driver;
+
+    public static void openBrowser() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(Config.readFromConfig("config.properties", "ui.url"));
+    }
+
+    public static void closeBrowser() {
+        driver.close();
+        driver.quit();
+    }
+}
---
+++ src/main/java/Bases/LoginPage.java
@@ -0,0 +1,33 @@
+package Bases;
+
+import org.magento.utilsclasses.FunctionsLibrary;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+    ChromeDriver driver;
+    FunctionsLibrary functionsLibrary;
+    @FindBy(id ="username")
+    WebElement userNameField;
+    @FindBy(name = "login[password]")
+    WebElement passwordField;
+    @FindBy(xpath = "//input[@title='Login']")
+    WebElement loginButton;
+
+    public LoginPage(ChromeDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+        functionsLibrary = new FunctionsLibrary(driver);
+    }
+
+    public void login(String userName, String password) {
+        functionsLibrary.waitForElementPresent(userNameField);
+        userNameField.sendKeys(userName);
+        functionsLibrary.waitForElementPresent(passwordField);
+        passwordField.sendKeys(password);
+        functionsLibrary.waitForElementPresent(loginButton);
+        loginButton.click();
+    }
+}
